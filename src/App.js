6c66b1986b7b13f53585be3b0682150e6ceb8edf
Raheem McDonald <raheemmcdonald@Raheems-MacBook-Pro.local>
2019-05-20 20:52:30 -0500

    Updates

--- src/App.js
+++ src/App.js
@@ -7,8 +7,10 @@ import HomeNavigation from './components/homeNavigation/HomeNavigation';
 import Home from './components/home/Home';
 import SignIn from './components/signIn/SignIn';
 
+import Dashboard from './components/dashboard/Dashbaord';
+
 // Views
-import Footer from './views/footer/Footer'
+import Footer from './views/footer/Footer';
 
 class App extends Component {
   render() {
@@ -18,6 +20,8 @@ class App extends Component {
         <Switch>
           <Route exact path='/' component={Home} />
           <Route exact path='/sign-in/' component={SignIn} />
+
+          <Route exact path='/dashboard/' component={Dashboard} />
         </Switch>
         <Route path='/' component={Footer} />
       </div>
---
+++ src/components/dashboard/Dashbaord.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+
+class Dashboard extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  componentDidMount() {
+    document.title = 'Dashboard | Console Membership Management Studio';
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <Switch />
+      </React.Fragment>
+    );
+  }
+}
+
+export default Dashboard;

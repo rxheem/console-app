8120c058d945d16f6a32675dc420c8e69473a411
Raheem McDonald <raheemmcdonald@Raheems-MacBook-Pro.local>
2019-05-23 21:18:00 -0500

    Fixed sign in

--- src/components/signIn/SignIn.js
+++ src/components/signIn/SignIn.js
@@ -36,84 +36,78 @@ class SignIn extends Component {
   render() {
     return (
       <Container id='SignIn' fluid={true}>
-        <Row>
-          <Col sm={12} md={6} lg={6}>
-            <span />
-          </Col>
-          {/* End first column */}
+        <div className='sign-in-header'>
+          <h4>Sign In</h4>
+          <span>Enter your credentials to get started</span>
+        </div>
+        <br />
 
-          <Col sm={12} md={6} lg={6}>
-            <div className='sign-in-header'>
-              <h4>Sign In</h4>
-              <span>Enter your credentials to get started</span>
-            </div>
-            <br />
+        <Form id='sign-in-form' action='' method='post'>
+          <Form.Group controlId='username'>
+            <span className='required'>* </span>
+            <Form.Label>Username or email</Form.Label>
+            <Form.Control
+              autoFocus
+              type='email'
+              placeholder='Enter username or email'
+              value={this.state.username}
+              onChange={this.handleChange.bind(this)}
+            />
+            <Form.Text className='text-muted'>
+              Emails are used for verification purposes only. We'll never share
+              your email with anyone else and we'll never send you spam
+            </Form.Text>
+          </Form.Group>
 
-            <Form id='sign-in-form' action=''>
-              <Form.Group controlId='username'>
-                <span className='required'>* </span>
-                <Form.Label>Username or email</Form.Label>
-                <Form.Control
-                  autoFocus
-                  type='email'
-                  placeholder='Enter username or email'
-                  value={this.state.username}
-                  onChange={this.handleChange.bind(this)}
-                />
-                <Form.Text className='text-muted'>
-                  Emails are used for verification purposes only. We'll never
-                  share your email with anyone else and we'll never send you
-                  spam
-                </Form.Text>
-              </Form.Group>
+          <Form.Group controlId='password'>
+            <span className='required'>* </span>
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type='password'
+              placeholder='Enter your password'
+              value={this.state.password}
+              onChange={this.handleChange.bind(this)}
+            />
+          </Form.Group>
 
-              <Form.Group controlId='password'>
-                <span className='required'>* </span>
-                <Form.Label>Password</Form.Label>
-                <Form.Control
-                  type='password'
-                  placeholder='Enter your password'
-                  value={this.state.password}
-                  onChange={this.handleChange.bind(this)}
-                />
-              </Form.Group>
+          <Form.Group controlId='churchCode'>
+            <span className='required'>* </span>
+            <Form.Label>Church Code</Form.Label>
+            <Form.Control
+              maxLength='5'
+              type='text'
+              placeholder='Enter church code'
+              value={this.state.churchCode}
+              onChange={this.handleChange.bind(this)}
+            />
+          </Form.Group>
 
-              <Form.Group controlId='churchCode'>
-                <span className='required'>* </span>
-                <Form.Label>Church Code</Form.Label>
-                <Form.Control
-                  maxLength='5'
-                  type='text'
-                  placeholder='Enter church code'
-                  value={this.state.churchCode}
-                  onChange={this.handleChange.bind(this)}
-                />
-              </Form.Group>
+          <Form.Group controlId='PIN'>
+            <Form.Label>Security PIN</Form.Label>
+            <Form.Control
+              maxLength='4'
+              type='text'
+              placeholder='Enter your 4 digit PIN'
+              value={this.state.PIN}
+              onChange={this.handleChange.bind(this)}
+              style={{ width: '200px' }}
+            />
+            <Form.Text className='text-muted'>
+              Only enter your PIN if you are in a secure area and never share
+              your PIN with anyone.
+            </Form.Text>
+          </Form.Group>
 
-              <Form.Group controlId='PIN'>
-                <Form.Label>Security PIN</Form.Label>
-                <Form.Control
-                  maxLength='4'
-                  type='text'
-                  placeholder='Enter your 4 digit PIN'
-                  value={this.state.PIN}
-                  onChange={this.handleChange.bind(this)}
-                  style={{ width: '200px' }}
-                />
-              </Form.Group>
+          <div className='password-help'>
+            <Link className='fogot-password ' to='/forgot-password/'>
+              Need help signing in?
+            </Link>
+          </div>
 
-              <div className='password-help'>
-                <Link className='fogot-password ' to='/forgot-password/'>
-                  Need help signing in?
-                </Link>
-              </div>
-
-              <Button primary onClick={this.handleSubmit}>
-                Sign In
-              </Button>
-            </Form>
-          </Col>
-        </Row>
+          <Button primary onClick={this.handleSubmit}>
+            Sign In
+          </Button>
+        </Form>
       </Container>
     );
   }
